606e3bfef293c1b69272c58530172236c9f6e1ea
SRayyan555 <rayyan@elred.io>
2026-05-04 23:35:12 +0530

    responsive sections

--- components/Different.jsx
+++ components/Different.jsx
@@ -1,14 +1,14 @@
 export default function Different() {
   return (
-    <section id="different" className="bg-white py-[100px]">
+    <section id="different" className="bg-white py-[50px] md:py-[100px]">
       <div className="flex flex-col items-center">
 
         <div className="w-full flex flex-col">
           {/* First Principles Row */}
-          <div className="w-full flex justify-center">
-            <div className="w-full md:px-[40px] px-[10px] mx-[60px] justify-between  flex border border-[#E8BEC1] md:pb-[60px] pb-[20px]  items-center">
+          <div className="flex justify-center">
+            <div className="md:px-[40px] w-full  md:mx-[60px] mx-[10px] justify-between  flex border-b border-[#E8BEC1] md:pb-[60px] pb-[20px]  items-center">
               {/* Left Side: Title */}
-              <div className="flex justify-center md:justify-end items-center w-[308px]">
+              <div className="flex justify-between md:justify-end items-center md:w-[308px] w-[120px]">
                 <span className="font-serif italic text-[18px] md:text-[45px] text-[#A3A3A3] leading-none">
                   First
                 </span>
@@ -18,18 +18,18 @@ export default function Different() {
               </div>
 
               {/* Right Side: Content */}
-              <div className="flex flex-col gap-8 items-start max-w-[650px] w-[220px] md:w-[650px]">
+              <div className="flex flex-col md:gap-8 gap-[8px] items-start max-w-[650px] w-[220px] md:w-[650px]">
                 <p className="font-inter italic md:text-[32px] text-[12px] leading-[160%] text-black ">
                   A series of <span className="font-bold italic">problem solving</span> workshops held by the worlds best professors
                 </p>
 
                 <div className="flex items-center md:gap-4 gap-[8px] text-[#A3A3A3]">
-                <img src="/assets/Group.svg" alt="Calendar" className="md:w-[27px] md:h-[27px] w-[16px] h-[16px]" />
+                  <img src="/assets/Group.svg" alt="Calendar" className="md:w-[27px] md:h-[27px] w-[16px] h-[16px]" />
                   <span className="font-display text-[#656A6B] font-medium leading-[120%] tracking-[0px] text-[14px] md:text-[28px]">
-                    <span className=" font-bold">#6</span>/year
+                    <span className="text-[14px] md:text-[28px] font-bold">#6</span>/year
                   </span>
                 </div>
-              </div> 
+              </div>
             </div>
           </div>
 
--- components/WhatWeAre.jsx
+++ components/WhatWeAre.jsx
@@ -1,14 +1,14 @@
 export default function WhatWeAre() {
-  const headingStyle = "font-display font-semibold text-[72px] leading-[120%] tracking-[-2.6px] text-[#333333] text-center";
-  const paragraphStyle = "font-inter font-medium text-[32px] leading-[130%] tracking-[0px] text-[#333333] text-center max-w-[540px] mx-auto";
+  const headingStyle = "font-display font-semibold md:text-[72px] text-[25px] leading-[120%] md:tracking-[-2.6px] tracking-[-1px] text-[#333333] text-center";
+  const paragraphStyle = "font-inter font-medium text-[12px] md:text-[32px] leading-[130%] tracking-[0px] text-[#333333] text-center max-w-[540px] mx-auto";
 
   return (
-    <section id="what-we-are" className="pb-[100px] bg-white flex flex-col items-center gap-[100px] border-b border-[#67686B]">
+    <section id="what-we-are" className="md:pb-[100px] pb-[40px] bg-white flex flex-col items-center md:gap-[100px] gap-[50px] border-b border-[#67686B]">
       {/* What we are Section */}
       <div className="w-full max-w-[1280px] px-8 flex flex-col gap-[40px]">
         <h2 className={headingStyle}>What we are</h2>
-        
-        <div className="grid md:grid-cols-2 gap-x-20 gap-y-24 items-start">
+
+        <div className="grid grid-cols-2 gap-x-8 md:gap-x-20 gap-y-12 md:gap-y-24 items-start">
           <div className="flex flex-col">
             <p className={paragraphStyle}>
               A network of professionals from the best companies. You are who you spend time with - fueling motivation.
@@ -26,20 +26,20 @@ export default function WhatWeAre() {
       </div>
 
       {/* What we are not Section */}
-      <div className="w-full max-w-[1280px] px-8 flex flex-col gap-[40px]">
+      <div className="w-full max-w-[1280px] px-8 flex flex-col md:gap-[40px] gap-[20px]">
         <h2 className={headingStyle}>
           What we are <span className="text-[#E31E24]">not</span>
         </h2>
 
-        <div className="flex justify-between items-center px-[40px]">
+        <div className="flex justify-between items-center md:px-[40px]">
           <div className=" ">
-            <h3 className=" font-display font-semibold text-[40px] leading-[120%] tracking-[-1.5px] text-[#333333] text-left max-w-[400px]">
+            <h3 className=" font-display font-semibold md:text-[40px] text-[16px] leading-[120%] md:tracking-[-1.5px] tracking-[0px] text-[#333333] text-left max-w-[400px]">
               Not executive education
             </h3>
           </div>
           <div className="flex md:pl-20">
-            <p className="font-sans md:w-[650px]  font-medium text-[32px] leading-[120%] tracking-[0px] text-[#333333] ">
-              Anyone can organise executive education,<br/> its the peer group that matters.
+            <p className="font-sans md:w-[650px] w-[220px]  font-medium md:text-[32px] text-[12px] leading-[120%] tracking-[0px] text-[#333333] ">
+              Anyone can organise executive education, its the peer group that matters.
             </p>
           </div>
         </div>

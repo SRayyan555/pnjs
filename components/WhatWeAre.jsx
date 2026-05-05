135dbfff6941ee043e5b7b7186d1c42ff48b7952
SRayyan555 <rayyan@elred.io>
2026-05-05 22:00:00 +0530

    made changes in tailwind props

--- components/WhatWeAre.jsx
+++ components/WhatWeAre.jsx
@@ -3,9 +3,9 @@ export default function WhatWeAre() {
   const paragraphStyle = "font-inter font-medium text-[12px] md:text-[32px] leading-[130%] tracking-[0px] text-[#333333] text-center max-w-[540px] mx-auto";
 
   return (
-    <section id="what-we-are" className=" md:pb-[100px] pb-[40px] bg-white flex flex-col items-center md:gap-[100px] gap-[50px]">
+    <section id="what-we-are" className=" md:pb-25 pb-10 bg-white flex flex-col items-center md:gap-[100px] gap-[50px]">
       {/* What we are Section */}
-      <div className="w-full max-w-[1280px] px-8 flex flex-col gap-[40px]">
+      <div className="w-full max-w-7xl px-8 flex flex-col gap-10">
         <h2 className={headingStyle}>What we are</h2>
 
         <div className="grid grid-cols-2 gap-x-8 md:gap-x-20 gap-y-12 md:gap-y-24 items-start">
@@ -14,7 +14,7 @@ export default function WhatWeAre() {
               A network of professionals from the best companies. You are who you spend time with - fueling motivation.
             </p>
           </div>
-          <div className="flex flex-col gap-[40px]">
+          <div className="flex flex-col gap-10">
             <p className={paragraphStyle}>
               We are focused on solving problems, so the DNA of everything we do revolves around it.
             </p>
@@ -26,7 +26,7 @@ export default function WhatWeAre() {
       </div>
 
       {/* What we are not Section */}
-      <div className="w-full max-w-[1280px] px-8 flex flex-col md:gap-[40px] gap-[20px]">
+      <div className="w-full max-w-[1280px] px-8 flex flex-col md:gap-10 gap-[20px]">
         <h2 className={headingStyle}>
           What we are <span className="text-[#E31E24]">not</span>
         </h2>

f9f7000ac2f619687bfa37a65d9afd83f23a19b5
TashafM <thecomputerpitch@gmail.com>
2026-05-08 11:06:04 +0530

    new

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,8 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import Image from 'next/image';
+import logo from '../public/assets/logo.svg'
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -29,8 +31,7 @@ export default function Navbar() {
     <>
       <nav className="flex justify-between items-start p-8 w-full absolute top-0 left-0 z-40">
         <div className="flex flex-col leading-tight cursor-pointer" onClick={() => navigateTo("/")}>
-          <span className="text-4xl font-black text-red-600 tracking-tighter">CSuite</span>
-          <span className="text-4xl font-black text-zinc-500 tracking-tighter -mt-2">Network</span>
+          <Image src={logo} alt="professional network"/>
         </div>
         <button 
           onClick={toggleMenu}
